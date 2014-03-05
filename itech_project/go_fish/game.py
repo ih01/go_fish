8e5bd8600569a0bf8e2ed7365fb78fa6020e9c90
ScottMcc <simccorkell@gmail.com>
2014-03-05 22:13:04 +0000

    Game class updated, game_control added

--- itech_project/go_fish/game.py
+++ itech_project/go_fish/game.py
@@ -1,57 +1,61 @@
-#game logic lives and operates here **un-finished, dont try to use yet**
-#basic version 1, just gives random number of fish
-#game board state not stored
+#game state lives here
+#processes movement and fishing
+#version 1, just gives random number of fish
 
 #import data needed from other models
+import os
+from itech_project import settings
 from random import randint
+from django.core.management import setup_environ
+setup_environ(settings)
 from models import UserProfile, Rod, Boat, Bait
 
 #Create and store a new game
 #with default values of 0, standard equipment
 #starting pos 0,0
+#In-game time given 8 hours (480 minutes)
 
-class CurrentGame(object):
+class MakeGame(object):
 	#defining new game
 	def __init__(self,
-		TIME_LEFT = 480,
-		POSX = 0,
-		POSY = 0
-		TOTAL_FISH = 0)
+		TIME_REMAINING = 480,
+		X_COORD = 0,
+		Y_COORD = 0,
+		TOTAL_FISH = 0):
+		
+		self.time_left = TIME_REMAINING
+		self.posX = X_COORD
+		self.posY = Y_COORD
+		self.fishTotal = TOTAL_FISH
+		
+	#basic time used to fish - FISH_TIME
+	#basic per square move time - BASIC_MOVE
+	FISH_TIME = 15
+	BASIC_MOVE = 60
 	
-
-#constants are:
-#basic fish time FISH_TIME
-#basic per square move time BASIC_MOVE
-
-#Instancees
-#TIME_LEFT 
-#POSX  x co-ordinate
-#POSY = y co-ordinate
-#TOTAL_FISH
-
-
 
 	#function for calculating movement and decreasing time
 	#location will come in as a parameter
-	#BOAT_MULT comes from boat class, via the user profile
-	#need to make sure correct data types being used
+	#needs to be passed user object
+	#timeMod comes from boat class
 
-	def move(newX, newY)
-		DISTANCE = abs(newX - POSX)
-		DISTANCE += abs(newY - POSY)
-
-		used_boat = Boat.objects.get(Boat.name__exact=User.boat)
+	def move(self, user, newX, newY):
+		#calculate distance moved
+		distance = abs(newX - self.posX)
+		distance += abs(newY - self.posY)
 	
-		MOVE_TIME = DISTANCE*BASIC_MOVE*(used_boat.timeMod)
+		#access users profile to get current equipment
+		user_prof = UserProfile.objects.get(user=user)
+			
+		#calculate move time
+		move_time = distance*self.BASIC_MOVE*(user_prof.boat.timeMod)
 	
-		# check enough time left(not sure if need this yet)
-		if MOVE_TIME>TIME_LEFT:
-			#do something to say unable to move
-		else:
-			TIME_LEFT -= MOVE_TIME
-			POSX = newX
-			POSY = newY
-			#data updated, pickle here?
+		#update time left
+		self.time_left -= move_time
+	
+		#update position.
+		self.posX = newX
+		self.posY = newY
 
 	
 	
@@ -59,9 +63,25 @@ class CurrentGame(object):
 	#for now just dish out random number
 	#fish generation being handled seperately
 
-	def fish()
-		TIME_LEFT -= FISH_TIME
-		return (randint(1, 20))*ROD_MULT*BAIT_MULT
+	def fish(user):
+
+		#reduce time left by fishing time
+		self.time_left -= self.FISH_TIME
+		
+		#access users profile to get current equipment
+		user_prof = UserProfile.objects.get(user=user)
+		
+		#call to fish generation will go here
+		basic_fish = (randint(1, 20)
+		
+		#apply multipliers
+		fish_caught = basic_fish*(user_prof.rod.fishMod)*(user_prof.bait.fishMod)
+
+		#update fish total
+		self.fishTotal += fish_caught
+
+		#return the fish caught this round
+		return fish_caught
 
 
 	
---
+++ itech_project/go_fish/game_control.py
@@ -0,0 +1,65 @@
+#functions for controlling
+#loading/saving and creation of games
+#all functions pickle data to
+#or unpickle from the DB
+# -- not yet tested --
+
+#import data needed from other models
+#may not be needed if calling module has
+#already done this?
+import os
+from itech_project import settings
+from random import randint
+from django.core.management import setup_environ
+setup_environ(settings)
+from models import Game
+from game import CurrentGame
+#imported as pickle just in case someone
+#wants to revert to regular pickle later
+import cPickle as pickle
+
+#create a new game for the user
+#and store it in the DB
+def new_game(user):
+
+	#see if duplicate game check needed here
+	#create a new game
+	newGame = MakeGame()
+
+	#pickle the object into a string
+	picGame = pickle.dumps(newGame)
+	
+	#Store in the DB
+	Game.objects.get_or_create(user=user, pickledgame=picGame)
+	
+
+
+#loads a game from the DB and returns the game object
+def load_game(user):
+	
+	#get the users game
+	users_game = Game.objects.get(user=user)
+
+	#unpickle it back to an object and return it
+	game = pickle.loads(users_game.pickledgame)
+	return game
+	
+	
+
+#stores a users game back to the DB
+def save_game(user, game):
+	
+	#pickle the game
+	saveGame = pickle.dumps(game)
+	
+	#find the correct model object in the DB
+	users_game = Game.objects.get(user=user)
+
+	#update it with new game state and save to DB
+	users_game.pickledgame = saveGame
+	users_game.save()
+	
+
+
+
+	
