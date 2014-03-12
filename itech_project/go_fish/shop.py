99ac231ae6619f8c962ea8385f9956bb7ad08bee
unknown <2111188H@BO1028-2-03.ad.dcs.gla.ac.uk>
2014-03-12 15:11:07 +0000

    just another change to the indentation of shop.py

--- itech_project/go_fish/shop.py
+++ itech_project/go_fish/shop.py
@@ -11,12 +11,12 @@ class Shop(object):
     def buyItem(self, item, user):
 
         if not ( self.balance > item.cost ):  # user has insufficient means
-        return
+			return
 
         else:  # calculate new balance
-        newBalance = self.balance - item.cost
+			newBalance = self.balance - item.cost
 
-        update = user.get_profile()
+			update = user.get_profile()
 
     # update user balance
         update.balance = newBalance
